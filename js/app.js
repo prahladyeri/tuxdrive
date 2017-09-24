eaa9ab0c0a5016c173f457fa498aa7a9bcc2f65b
Prahlad Yeri <prahladyeri@yahoo.com>
2017-09-24 15:10:10 +0530

    updated project home

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
 var appName = "Tux Drive";
 var repoName = "prahladyeri/tuxdrive";
 
-$(document).ready(function(){
-	$(".appName").text(appName);
-	$(".appURL").attr('href','https://github.com/' + repoName);
-	GetAllReleases();
-});
-
-
-function GetAllReleases() {
+function getAllReleases() {
 	console.log('here');
 	$.getJSON("https://api.github.com/repos/" + repoName + "/releases")
 	.done(function(releases){
@@ -23,6 +16,9 @@ function GetAllReleases() {
 			$div.find('.card-title').text("Version " + releases[i]['tag_name'] + " of " + appName + " has been Released.");
 			$div.find('.btn-binary').attr('href', releases[i].assets[0].browser_download_url);
 			$div.find('.btn-src').attr('href', releases[i].tarball_url);
+			if (i==0) {
+				$div.find('.btn-binary').attr('name','btnLatest');
+			}
 			
 			//$div.append("<span class='info'>");
 			//$div.find('.info').text("Version " + releases[i]['tag_name'] + " of " + appName + " has been Released!");
@@ -35,28 +31,32 @@ function GetAllReleases() {
 }
 
 //https://stackoverflow.com/questions/24987542/is-there-a-link-to-github-for-downloading-a-file-in-the-latest-release-of-a-repo
-function GetLatestReleaseInfo() {
+function getLatestDownloadURL(func) {
+	console.log('getting latest url');
 	$.getJSON("https://api.github.com/repos/" + repoName + "/releases/latest").done(function (release) {
+		//console.log('success',release);
 		var asset = release.assets[0];
-		var downloadCount = 0;
-		for (var i = 0; i < release.assets.length; i++) {
-			downloadCount += release.assets[i].download_count;
-		}
-		var oneHour = 60 * 60 * 1000;
-		var oneDay = 24 * oneHour;
-		var dateDiff = new Date() - new Date(asset.updated_at);
-		var timeAgo;
-		if (dateDiff < oneDay)
-		{
-			timeAgo = (dateDiff / oneHour).toFixed(1) + " hours ago";
-		}
-		else
-		{
-			timeAgo = (dateDiff / oneDay).toFixed(1) + " days ago";
-		}
-		var releaseInfo = release.name + " was updated " + timeAgo + " and downloaded " + downloadCount.toLocaleString() + " times.";
-		$(".btn-download").attr("href", asset.browser_download_url);
-		$(".release-info").text(releaseInfo);
-		$(".release-info").fadeIn("slow");
+		//return asset.browser_download_url;
+		func(asset.browser_download_url);
+		//~ var downloadCount = 0;
+		//~ for (var i = 0; i < release.assets.length; i++) {
+			//~ downloadCount += release.assets[i].download_count;
+		//~ }
+		//~ var oneHour = 60 * 60 * 1000;
+		//~ var oneDay = 24 * oneHour;
+		//~ var dateDiff = new Date() - new Date(asset.updated_at);
+		//~ var timeAgo;
+		//~ if (dateDiff < oneDay)
+		//~ {
+			//~ timeAgo = (dateDiff / oneHour).toFixed(1) + " hours ago";
+		//~ }
+		//~ else
+		//~ {
+			//~ timeAgo = (dateDiff / oneDay).toFixed(1) + " days ago";
+		//~ }
+		//~ var releaseInfo = release.name + " was updated " + timeAgo + " and downloaded " + downloadCount.toLocaleString() + " times.";
+		//~ $(".btn-download").attr("href", asset.browser_download_url);
+		//~ $(".release-info").text(releaseInfo);
+		//~ $(".release-info").fadeIn("slow");
 	});
 }
